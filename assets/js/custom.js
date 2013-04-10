a20d5f3a0ed98d682822777661978e164ee05de0
wclifton1 <wclifton1@gmail.com>
2013-04-09 22:25:14 -0500

    jquery no conflict was causing problems

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -231,3 +231,14 @@ $(document).ready(function() {
         Page.init();
       
       });
+
+/* meetup widget */
+
+var mup_widget = {
+    with_jquery: function(block) {
+        block(jQuery, document.body);
+    },
+    api_call: function(path, params) {
+        return "http://api.meetup.com" + path + "?callback=?&" + jQuery.param(jQuery.extend({ key: $api_key }, params));
+    }
+};
