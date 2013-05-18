cd3f9960f1e8771af28c6e16a2934b208f288d06
wclifton1 <wclifton1@gmail.com>
2013-05-18 10:31:55 -0500

    Revert "Revert "Revert "indents"""
    This reverts commit 4a254d2d85a15054b6e846f2371d9cc5e09e5621.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,52 +7,52 @@ $(document).ready(function() {
     $(".todo li").click(function() {
         $(this).toggleClass("todo-done");
     });
-	
+
 });
 
 
 
 /* -------------------- Sticky nav --------------------- */
 /*
-function UpdateTableHeaders() {
-$(".persist-area").each(function() {
-
-var el             = $(this),
-offset         = el.offset(),
-scrollTop      = $(window).scrollTop(),
-floatingHeader = $(".floatingHeader", this)
-
-if ((scrollTop > offset.top) && (scrollTop < offset.top + el.height())) {
-floatingHeader.css({
-"visibility": "visible"
-});
-} else {
-floatingHeader.css({
-"visibility": "hidden"
-});      
-};
-});
-}
-
-// DOM Ready      
-$(function() {
-
-var clonedHeaderRow;
-
-$(".persist-area").each(function() {
-clonedHeaderRow = $(".persist-header", this);
-clonedHeaderRow
-.before(clonedHeaderRow.clone())
-.css("width", clonedHeaderRow.width())
-.addClass("floatingHeader");
-
-});
-
-$(window)
-.scroll(UpdateTableHeaders)
-.trigger("scroll");
-
-});
+    function UpdateTableHeaders() {
+       $(".persist-area").each(function() {
+       
+           var el             = $(this),
+               offset         = el.offset(),
+               scrollTop      = $(window).scrollTop(),
+               floatingHeader = $(".floatingHeader", this)
+           
+           if ((scrollTop > offset.top) && (scrollTop < offset.top + el.height())) {
+               floatingHeader.css({
+                "visibility": "visible"
+               });
+           } else {
+               floatingHeader.css({
+                "visibility": "hidden"
+               });      
+           };
+       });
+    }
+    
+    // DOM Ready      
+    $(function() {
+    
+       var clonedHeaderRow;
+    
+       $(".persist-area").each(function() {
+           clonedHeaderRow = $(".persist-header", this);
+           clonedHeaderRow
+             .before(clonedHeaderRow.clone())
+             .css("width", clonedHeaderRow.width())
+             .addClass("floatingHeader");
+             
+       });
+       
+       $(window)
+        .scroll(UpdateTableHeaders)
+        .trigger("scroll");
+       
+    });
 
 
 
@@ -60,82 +60,82 @@ $(window)
 
 /* -------------------- Isotope --------------------- */
 
-$(function(){
-	
-	var $container = $('#container');
-	
-	$container.isotope({
+    $(function(){
+      
+      var $container = $('#container');
+      
+      $container.isotope({
         itemSelector : '.element',
-		  getSortData : {
-			  symbol : function( $elem ) {
-				  return $elem.attr('data-symbol');
-			  },
-			  category : function( $elem ) {
-				  return $elem.attr('data-category');
-			  },
-			  number : function( $elem ) {
-				  return parseInt( $elem.find('.number').text(), 10 );
-			  },
-			  weight : function( $elem ) {
-				  return parseFloat( $elem.find('.weight').text().replace( /[\(\)]/g, '') );
-			  },
-			  name : function ( $elem ) {
-				  return $elem.find('.name').text();
-			  }
-		  }
+        getSortData : {
+          symbol : function( $elem ) {
+            return $elem.attr('data-symbol');
+          },
+          category : function( $elem ) {
+            return $elem.attr('data-category');
+          },
+          number : function( $elem ) {
+            return parseInt( $elem.find('.number').text(), 10 );
+          },
+          weight : function( $elem ) {
+            return parseFloat( $elem.find('.weight').text().replace( /[\(\)]/g, '') );
+          },
+          name : function ( $elem ) {
+            return $elem.find('.name').text();
+          }
+        }
       });
-		
-		
-		var $optionSets = $('#options .option-set'),
-			$optionLinks = $optionSets.find('a');
-		
-		$optionLinks.click(function(){
-			var $this = $(this);
-			// don't proceed if already selected
-			if ( $this.hasClass('selected') ) {
-				return false;
-			}
-			var $optionSet = $this.parents('.option-set');
-			$optionSet.find('.selected').removeClass('selected');
-			$this.addClass('selected');
-			
-			// make option object dynamically, i.e. { filter: '.my-filter-class' }
-			var options = {},
-				key = $optionSet.attr('data-option-key'),
-				value = $this.attr('data-option-value');
-			// parse 'false' as false boolean
-			value = value === 'false' ? false : value;
-			options[ key ] = value;
-			if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-				// changes in layout modes need extra logic
-				changeLayoutMode( $this, options )
-			} else {
-				// otherwise, apply new options
-				$container.isotope( options );
-			}
-			
-			return false;
-		});
-		
-		
+      
+      
+      var $optionSets = $('#options .option-set'),
+          $optionLinks = $optionSets.find('a');
+
+      $optionLinks.click(function(){
+        var $this = $(this);
+        // don't proceed if already selected
+        if ( $this.hasClass('selected') ) {
+          return false;
+        }
+        var $optionSet = $this.parents('.option-set');
+        $optionSet.find('.selected').removeClass('selected');
+        $this.addClass('selected');
+  
+        // make option object dynamically, i.e. { filter: '.my-filter-class' }
+        var options = {},
+            key = $optionSet.attr('data-option-key'),
+            value = $this.attr('data-option-value');
+        // parse 'false' as false boolean
+        value = value === 'false' ? false : value;
+        options[ key ] = value;
+        if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
+          // changes in layout modes need extra logic
+          changeLayoutMode( $this, options )
+        } else {
+          // otherwise, apply new options
+          $container.isotope( options );
+        }
+        
+        return false;
+      });
+
+      
     });
 
 
 
 /* -------------------- Tweet --------------------- */
 
-jQuery(function($){
-	$(".tweet").tweet({
-		username: "enventuremed",
-		join_text: "auto",
-		avatar_size: 36,
-		count: 3,
-		/*
-auto_join_text_default: "we said,",
-auto_join_text_ed: "we",
-auto_join_text_ing: "we were",
-auto_join_text_reply: "we replied to",
-auto_join_text_url: "we were checking out",
+    jQuery(function($){
+        $(".tweet").tweet({
+            username: "enventuremed",
+            join_text: "auto",
+            avatar_size: 36,
+            count: 3,
+			/*
+            auto_join_text_default: "we said,",
+            auto_join_text_ed: "we",
+            auto_join_text_ing: "we were",
+            auto_join_text_reply: "we replied to",
+            auto_join_text_url: "we were checking out",
 */
             loading_text: "loading tweets..."
         });
@@ -146,71 +146,71 @@ auto_join_text_url: "we were checking out",
 
 /* -------------------- Slider --------------------- */
 
-$(function() {
-	
-	var Page = (function() {
-		
-		var $navArrows = $( '#nav-arrows' ),
+      $(function() {
+      
+        var Page = (function() {
+
+          var $navArrows = $( '#nav-arrows' ),
             $nav = $( '#nav-dots > span' ),
-			  slitslider = $( '#slider' ).slitslider( {
-				  onBeforeChange : function( slide, pos ) {
-					  
-					  $nav.removeClass( 'nav-dot-current' );
-					  $nav.eq( pos ).addClass( 'nav-dot-current' );
-					  
-				  }
-			  } ),
-			  
-			  init = function() {
-				  
-				  initEvents();
-				  
-			  },
-			  initEvents = function() {
-				  
-				  // add navigation events
-				  $navArrows.children( ':last' ).on( 'click', function() {
-					  
-					  slitslider.next();
-					  return false;
-					  
-				  } );
-				  
-				  $navArrows.children( ':first' ).on( 'click', function() {
-					  
-					  slitslider.previous();
-					  return false;
-					  
-				  } );
-				  
-				  $nav.each( function( i ) {
-					  
-					  $( this ).on( 'click', function( event ) {
-						  
-						  var $dot = $( this );
-						  
-						  if( !slitslider.isActive() ) {
-							  
-							  $nav.removeClass( 'nav-dot-current' );
-							  $dot.addClass( 'nav-dot-current' );
-							  
-						  }
-						  
-						  slitslider.jump( i + 1 );
-						  return false;
-						  
-					  } );
-					  
-				  } );
-				  
-			  };
-			
+            slitslider = $( '#slider' ).slitslider( {
+              onBeforeChange : function( slide, pos ) {
+
+                $nav.removeClass( 'nav-dot-current' );
+                $nav.eq( pos ).addClass( 'nav-dot-current' );
+
+              }
+            } ),
+
+            init = function() {
+
+              initEvents();
+              
+            },
+            initEvents = function() {
+
+              // add navigation events
+              $navArrows.children( ':last' ).on( 'click', function() {
+
+                slitslider.next();
+                return false;
+
+              } );
+
+              $navArrows.children( ':first' ).on( 'click', function() {
+                
+                slitslider.previous();
+                return false;
+
+              } );
+
+              $nav.each( function( i ) {
+              
+                $( this ).on( 'click', function( event ) {
+                  
+                  var $dot = $( this );
+                  
+                  if( !slitslider.isActive() ) {
+
+                    $nav.removeClass( 'nav-dot-current' );
+                    $dot.addClass( 'nav-dot-current' );
+                  
+                  }
+                  
+                  slitslider.jump( i + 1 );
+                  return false;
+                
+                } );
+                
+              } );
+
+            };
+
             return { init : init };
-			
+
         })();
-		  
-		  Page.init();
-		  
+
+        Page.init();
+      
       });
 
 /* meetup widget */
@@ -226,24 +226,24 @@ var mup_widget = {
 
 /* smooth scroll*/
 /*
-!function ($) {
-$('a[href^="#"]').bind('click.smoothscroll',function (e) {
-e.preventDefault();
-var target = this.hash;
-$target = $(target);
-$('html, body').stop().animate({
-'scrollTop': $target.offset().top
-}, 500, 'swing', function () {
-window.location.hash = target;
-});
-});
-}(window.jQuery)
-
-
+      !function ($) {
+        $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+            e.preventDefault();
+            var target = this.hash;
+                $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top
+            }, 500, 'swing', function () {
+                window.location.hash = target;
+            });
+        });
+      }(window.jQuery)
+		 
+		  
 /*scrollspy call*/
 $('a[href^="#"]').bind('click', function(e) {
-	e.preventDefault();
-	$('html, body').animate({ scrollTop: $(this.hash).offset().top }, 600);
-	
-	// edit: Opera requires the "html" elm. animated
+   e.preventDefault();
+   $('html, body').animate({ scrollTop: $(this.hash).offset().top }, 600);
+
+   // edit: Opera requires the "html" elm. animated
 });
